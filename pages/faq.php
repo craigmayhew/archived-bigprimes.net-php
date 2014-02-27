50dfda512e4701094a629a9a0c4eca86a4959b69
Craig Mayhew <craig@mayhew.io>
2014-02-27 22:49:29 +0000

    Added % break down to FAQ

--- pages/faq.php
+++ pages/faq.php
@@ -21,7 +21,15 @@
 <b>What is the first set of 100 numbers (e.g. 100-199, 1300-1399, 312300-312399) that does not contain a prime number?</b>
 <p>The first occurrence is between the prime numbers 1671781 and 1671907 on <a href="/archive/prime/1262/">this</a> page (near bottom of 3rd column).</p>
 
-
 <br /><br />
 <b>What is the smallest prime number that contains all digits 0 to 9 in sequence?</b>
 <p>100123456789<p>
+
+<br /><br />
+<b>What percentage of prime numbers end in 1,3,7 and 9?</b>
+<p>
+  25% of prime numbers end in a 1<br>
+  25% of prime numbers end in a 3<br>
+  25% of prime numbers end in a 7<br>
+  25% of prime numbers end in a 9<br>
+</p>
