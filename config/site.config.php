79b5a949e4054565f8341ed57244922d0ebc2beb
Craig Mayhew <craig@mayhew.io>
2014-02-27 22:57:14 +0000

    Updated contact email address in config

--- config/site.config.php
+++ config/site.config.php
@@ -8,7 +8,7 @@ $config['db']['host'] = 'localhost';
 $config['db']['pass'] = 'rambocheese666';
 $config['db']['db']   = 'bigprime_db';
 
-$config['email'] = 'craig@adire.co.uk';
+$config['email'] = 'craig@mayhew.io';
 //Constants
 define('SALT','LIME' );
 define('NEWLINE',"\r\n" );
