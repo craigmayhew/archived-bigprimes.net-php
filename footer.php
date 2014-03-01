a16c7f4437b36cd84d9dc59a5994abf4860c08e7
Craig Mayhew <craig@mayhew.io>
2014-03-01 19:10:49 +0000

    Updated hosting link

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </td>
 </tr>
 </table>
-<div align="center"><br /><br /><a href="http://www.adire.co.uk/" class="sidebarlink">Web design &amp; Hosting by Adire</a></div>
+<div align="center"><br /><br /><a href="http://www.adire.co.uk/" class="sidebarlink">Hosted by Adire</a></div>
 </body>
 </html>
