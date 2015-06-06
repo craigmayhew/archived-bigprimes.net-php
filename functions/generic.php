95c68fc103ba2a3b76424cbe1923daf8b89d2ae9
Craig Mayhew <craig@mayhew.io>
2015-06-06 17:07:06 +0100

    Removed days() from codebase

--- functions/generic.php
+++ functions/generic.php
@@ -1,12 +1,4 @@
 <?php
-//This returns an array of numbers from 1 to 31
-function days($all = false){
-    $array = array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31);
-    if($all == true){
-        $array = beginningArray($array,"All");
-    }
-    return $array;
-}
 //This returns an array of months
 function months($all = false,$numeric = true){
     if($numeric){
