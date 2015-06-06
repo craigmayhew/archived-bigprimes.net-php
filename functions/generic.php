8fdccfb986c7be7b1d08ea046b09991343cfb9f3
Craig Mayhew <craig@mayhew.io>
2015-06-06 17:07:55 +0100

    Removed months() from codebase

--- functions/generic.php
+++ functions/generic.php
@@ -1,32 +1,4 @@
 <?php
-//This returns an array of months
-function months($all = false,$numeric = true){
-    if($numeric){
-        $array = array(1,2,3,4,5,6,7,8,9,10,11,12);
-        if($all == true){
-            $array = beginningArray($array,"All");
-        }
-    }else{   
-        $array = array(
-                    array(1,"January"),
-                    array(2,"February"),
-                    array(3,"March"),
-                    array(4,"April"),
-                    array(5,"May"),
-                    array(6,"June"),
-                    array(7,"July"),
-                    array(8,"August"),
-                    array(9,"September"),
-                    array(10,"October"),
-                    array(11,"November"),
-		    array(12,"December")
-	         );
-        if($all == true){
-            $array = beginningArray($array,array(0,"All"));
-        }
-    }
-    return $array;
-}
 
 //used to make invalid xml into valid xml .. e.g. sanitize data from database
 function safeXML($str){
