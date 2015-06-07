bf44225acc8c8281205462667b6dd717b6822a5a
craigmayhew <craig@mayhew.io>
2015-06-07 15:20:30 +0100

    Moved calcperfect.php to tools folder. Updated downloads.php to be a pages class.

--- site/classes/pages/downloads.php
+++ site/classes/pages/downloads.php
@@ -3,69 +3,58 @@ namespace pages;
 
 class downloads extends \pages{
   function getContent(){
-$downloads_area_array[] = array(
-	'title' => 'All 44 known Mersenne primes',
-	'description' => 'This contains the first 44 Mersenne prime numbers.',
-	'link' => '/pages/archive/mersenne/Mersenne Primes'
-);
-$downloads_area_array[] = array(
-	'title' => 'All 12 factored Fermat numbers',
-	'description' => 'This contains the first 12 Fermat numbers.',
-	'link' => '/pages/archive/fermat/Fermat Numbers'
-);
-$downloads_area_array[] = array(
-	'title' => 'All 44 known perfect numbers',
-	'description' => 'This contains the first 44 perfect numbers.',
-	'link' => '/pages/archive/perfect/Perfect Numbers'
-);
+    $return = '';
 
+    $downloads = array(
+      array(
+        'title' => 'All 44 known Mersenne primes',
+        'description' => 'This contains the first 44 Mersenne prime numbers.',
+        'link' => 'archive/mersenne/Mersenne Primes',
+        'sizezip' => '23.58MB'
+      ),
+      array(
+        'title' => 'All 12 factored Fermat numbers',
+        'description' => 'This contains the first 12 Fermat numbers.',
+        'link' => 'archive/fermat/Fermat Numbers',
+        'sizezip' => '1.12KB'
+      ),
+      array(
+        'title' => 'All 44 known perfect numbers',
+        'description' => 'This contains the first 44 perfect numbers.',
+        'link' => 'archive/perfect/Perfect Numbers',
+        'sizezip' => '48.13MB'
+      )
+    );
 
-
-echo
-"<h1>Downloads</h1>".
-"<table cellpadding=\"2\" cellspacing=\"0\" class=\"text table\" bgcolor=\"#e0faed\">".
- "<tr>".
-  "<td width=\"200\">&nbsp;</td>".
-  "<td width=\"200\">&nbsp;</td>".
-  "<td width=\"70\">Zip</td>".
-  "<td width=\"70\">Rar</td>".
- "</tr>";
- foreach ($downloads_area_array as $key => $value){
-	 echo
-	 '<tr>',
-	  '<td align="left">',$value['title'],'</td>',
-	  '<td align="left">',$value['description'],'</td>',
-	  '<td style="white-space: nowrap;">';
-	  $localLink = $_SERVER['DOCUMENT_ROOT'].$value['link'].'.zip';
-	  if (file_exists($localLink)){
-	  	echo
-	  	'<a href="',$value['link'],'.zip',"\" class=\"link\"><img src=\"http://static.bigprimes.net/imgs/file_types/zip_20.gif\" alt=\"\" />&nbsp;(".$this->dl_file_size($localLink).")</a>";
-	  }
-	  echo
-	  '</td>',
-	  '<td style="white-space: nowrap;">';
-	  $localLink = $_SERVER['DOCUMENT_ROOT'].$value['link'].'.rar';
-	  if (file_exists($localLink)){
-	  	echo
-	  	'<a href="',$value['link'],'.rar',"\" class=\"link\"><img src=\"http://static.bigprimes.net/imgs/file_types/rar_20.gif\" alt=\"\" />&nbsp;(".$this->dl_file_size($localLink).")</a>";
-	  }
-	  echo
-	  "</td>".
-	 "</tr>";
- }
-echo
-'</table>',
-'<br /><br />';
-  }
-  function dl_file_size($file){
-    $bytes = filesize($file);
-    if ($bytes > 1000000){
-      $size = round(($bytes/1000000),2).' Mb';
-    }elseif ($bytes > 1000){
-      $size = round(($bytes/1000),2).' Kb';
-    }else{
-      $size = $bytes.' b';
+    $return .=
+    '<h1>Downloads</h1>'.
+    '<table cellpadding="2" cellspacing="0" class="text table" bgcolor="#e0faed">'.
+    '<tr>'.
+      '<td width="200">&nbsp;</td>'.
+      '<td width="200">&nbsp;</td>'.
+      '<td width="70">Zip</td>'.
+      '<td width="70">7Zip</td>'.
+    '</tr>';
+    foreach ($downloads as $k => $value){
+      $return .=
+      '<tr>'.
+        '<td align="left">'.$value['title'].'</td>'.
+        '<td align="left">'.$value['description'].'</td>'.
+        '<td style="white-space: nowrap;">';
+          '<a href="http://static.bigprimes.net/'.$value['link'].'.zip" class="link">'.
+            '<img src="http://static.bigprimes.net/imgs/file_types/zip_20.gif" alt="" />&nbsp;('.$value['size'].')'.
+          '</a>';
+        '</td>'.
+        '<td>'.
+          'N/A'.
+        '</td>'.
+      '</tr>';
     }
-    return $size;
+    $return .=
+    '</table>'.
+    '<br /><br />';
+
+    return
+    $return;
   }
 }
