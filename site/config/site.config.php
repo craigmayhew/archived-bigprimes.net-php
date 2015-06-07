1a9d2525b9a82c470ed18866c45a51a1b94ba3cb
craigmayhew <craig@mayhew.io>
2015-06-07 13:10:15 +0100

    Refactored giant variables include into separate classes.

--- site/config/site.config.php
+++ site/config/site.config.php
@@ -13,4 +13,14 @@ $config['email'] = 'primes@bigprimes.net';
 define('SALT','LIME' );
 define('NEWLINE',"\r\n" );
 //Misc
-?>
+$table_color = '#e0faed';
+$max_len_prime = 11;
+$max_len_cube = 17;
+$max_len_square = 19;
+$max_len_triangle = 17;
+$max_len_convertion = 500; //converting the number to binary and hex ...
+$max_len_factorization = 6;
+$max_len_roman_numerals = 6;
+$max_len_egypt_numerals = 7;
+$max_len_chinese_numerals = 6;
+$max_len_babylonian_numerals = 13;
