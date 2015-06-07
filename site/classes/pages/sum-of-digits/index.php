b38fadd1f5107c635fd94076acdcf1f77eda52ca
craigmayhew <craig@mayhew.io>
2015-06-07 17:11:08 +0100

    Converted to class.

--- site/classes/pages/sum-of-digits/index.php
+++ site/classes/pages/sum-of-digits/index.php
@@ -1,25 +1,31 @@
 <?php
-for($i=2;$i<=9;$i++){
-    $sums = $sumOfDigits->get($i);
-    echo
-    '<h1>Sum of '.$i.' digit primes</h1>
-    <table class="sumOfDigits">
-    <thead>
-    <tr>
-        <td>Sum</td>
-        <td>count</td>
-    </tr>
-    </thead>
-    <tbod>';
-    foreach($sums as &$s){
-        echo
-        '<tr>
-            <td>',$s['sum'],'</td>
-            <td>',$s['count'],'</td>
-        </tr>';
+namespace pages\sumofdigits;
+
+class index extends \pages{
+  function getContent(){
+    for($i=2;$i<=9;$i++){
+      $sums = $sumOfDigits->get($i);
+      $return .=
+      '<h1>Sum of '.$i.' digit primes</h1>'.
+      '<table class="sumOfDigits">'.
+        '<thead>'.
+          '<tr>'.
+            '<td>Sum</td>'.
+            '<td>count</td>'.
+          '</tr>'.
+        '</thead>'.
+        '<tbody>';
+        foreach($sums as &$s){
+          $return .=
+          '<tr>'.
+              '<td>'.$s['sum'].'</td>'.
+              '<td>'.$s['count'].'</td>'.
+          '</tr>';
+        }
+        $return .=
+        '</tbody>'.
+      '</table>'.
+      '<br />';
     }
-    echo
-    '</tbody>
-    </table><br />';
+  }
 }
-?>
