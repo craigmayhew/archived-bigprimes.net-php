711016f53a3ff4947fa6528199b79976d2c0a291
Craig Mayhew <craig@mayhew.io>
2017-06-03 19:29:09 +0000

    Corrected paths

--- test/Pages/Fermats.php
+++ test/Pages/Fermats.php
@@ -1,14 +1,15 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../src/Bigprimes/SumOfDigits.php';
+require_once __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__.'/../../src/Bigprimes/SumOfDigits.php';
 
 class test_Pages_Fermats extends TestCase
 {
 	public function test_get()
 	{
-            $pageContact = \Bigprimes\Pages\Fermats()->getContent();
+            $fermats = new \Bigprimes\Pages\Fermats(new \stdClass());
+            $pageContent = $fermats->getContent();
             $this->assertContains('The Fermat Numbers', $pageContent);
 	}
 }
