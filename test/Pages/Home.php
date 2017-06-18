41154560d1613a05dba09df7720657759bfd54b9
Craig Mayhew <craig@mayhew.io>
2017-06-18 17:52:55 +0000

    Corrected misleading variable name

--- test/Pages/Home.php
+++ test/Pages/Home.php
@@ -8,8 +8,8 @@ class test_Pages_Home extends TestCase
 {
 	public function test_get()
 	{
-            $fermats = new \Bigprimes\Pages\Home(new \stdClass());
-            $pageContent = $fermats->getContent();
+            $home = new \Bigprimes\Pages\Home(new \stdClass());
+            $pageContent = $home->getContent();
             $this->assertContains('13th November 2016', $pageContent);
             $this->assertContains('13th May 2005', $pageContent);
 	}
