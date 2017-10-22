2d25fdf4159be9c575b7a1d48bf9227ae7817b69
Craig Mayhew <craig@mayhew.io>
2017-10-22 16:54:12 +0000

    Now deploys php lambda for the first time!

--- build.tf
+++ build.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "eu-west-2"
+  region     = "eu-west-1"
 }
 
 resource "null_resource" "build" {
@@ -10,7 +10,7 @@ resource "null_resource" "build" {
 
 resource "aws_cloudformation_stack" "api" {
   capabilities = ["CAPABILITY_IAM"]
-  depends_on = ["null_resource.build"]
+  depends_on = ["null_resource.build","aws_s3_bucket_object.lambda"]
   name = "bigprimes-api"
   on_failure = "DELETE"
   parameters {
@@ -19,7 +19,19 @@ resource "aws_cloudformation_stack" "api" {
     mysqlUser = "root"
     mysqlPassword = "123456789"
     mysqlDatabase = "bigprimes"
-    s3Bucket = "bigprimes"
+    s3Bucket = "bigprimes-created-by-cfterraform"
   }
   template_body = "${ file("cloudformation/api.yml") }"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "bigprimes-created-by-cfterraform"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_object" "lambda" {
+  bucket = "${aws_s3_bucket.b.bucket}"
+  key    = "lambdas/php.zip"
+  source = "/tmp/lambda.zip"
+  etag   = "${md5(file("/tmp/lambda.zip"))}"
 }
