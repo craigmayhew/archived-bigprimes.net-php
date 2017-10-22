978f61d3149cad06e66d36a6a443c6a840176ce3
Craig Mayhew <craig@mayhew.io>
2017-10-23 00:10:54 +0100

    incomplete setup of api gateway

--- build.tf
+++ build.tf
@@ -15,8 +15,19 @@ resource "null_resource" "build" {
 
 resource "aws_cloudformation_stack" "api" {
   capabilities = ["CAPABILITY_IAM"]
-  depends_on = ["null_resource.build","aws_s3_bucket_object.lambda"]
+  depends_on = ["aws_cloudformation_stack.lambdas"]
   name = "bigprimes-api"
+  on_failure = "DELETE"
+  parameters {
+    certificateARN = "arn:aws:acm:us-east-1:902420391845:certificate/eb2693d6-fe4a-4f9f-8ce5-d09ae037e627"
+  }
+  template_body = "${ file("cloudformation/api.yml") }"
+}
+
+resource "aws_cloudformation_stack" "lambdas" {
+  capabilities = ["CAPABILITY_IAM"]
+  depends_on = ["null_resource.build","aws_s3_bucket_object.lambda"]
+  name = "bigprimes-lambdas"
   on_failure = "DELETE"
   parameters {
     # replace this with mysql IAM!
@@ -26,7 +37,7 @@ resource "aws_cloudformation_stack" "api" {
     mysqlDatabase = "bigprimes"
     s3Bucket = "${var.bucket}"
   }
-  template_body = "${ file("cloudformation/api.yml") }"
+  template_body = "${ file("cloudformation/lambdas.yml") }"
 }
 
 resource "aws_s3_bucket" "b" {
