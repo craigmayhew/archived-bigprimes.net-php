6c23566ee14f562f319545607c43a46ec8ecd739
Craig Mayhew <craig@mayhew.io>
2017-10-22 17:29:48 +0100

    Merged terraform files. Added depends.

--- build.tf
+++ build.tf
@@ -1,11 +1,25 @@
+provider "aws" {
+  region     = "eu-west-2"
+}
+
 resource "null_resource" "build" {
   provisioner "local-exec" {
     command = "cd tools && php build.php"
   }
 }
 
-resource "null_resource" "deploy" {
-  provisioner "local-exec" {
-    command = "cd tools && php uploadToS3.php key=${var.access_key} secret=${var.secret_key}"
+resource "aws_cloudformation_stack" "api" {
+  capabilities = ["CAPABILITY_IAM"]
+  depends_on = ["null_resource.build"]
+  name = "bigprimes-api"
+  on_failure = "DELETE"
+  parameters {
+    # replace this with mysql IAM!
+    mysqlHost = "mysql.adire.co.uk"
+    mysqlUser = "root"
+    mysqlPassword = "123456789"
+    mysqlDatabase = "bigprimes"
+    s3Bucket = "bigprimes"
   }
+  template_body = "${ file("cloudformation/api.yml") }"
 }
