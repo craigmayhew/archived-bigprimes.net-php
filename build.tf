f77a8654586723c227dbc64769c831cb59ffc68d
Craig Mayhew <craig@mayhew.io>
2017-10-30 23:20:51 +0000

    mysql vars now passed to php, php.js is built on the fly

--- build.tf
+++ build.tf
@@ -2,10 +2,10 @@ variable "bucket" {
   type = "string"
   default = "bigprimes-created-by-cfterraform"
 }
-variable "rdshost" {}
-variable "rdsdb" {}
-variable "rdsuser" {}
-variable "rdspass" {}
+variable "rdshost" {type = "string"}
+variable "rdsdb" {type = "string"}
+variable "rdsuser" {type = "string"}
+variable "rdspass" {type = "string"}
 
 
 provider "aws" {
@@ -13,9 +13,53 @@ provider "aws" {
 }
 
 resource "null_resource" "build" {
+  depends_on = ["local_file.phpjs"]
   provisioner "local-exec" {
     command = "cd tools && php build.php"
   }
+}
+
+resource "local_file" "phpjs" {
+  content = <<EOF
+process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+
+const spawn = require('child_process').spawn;
+
+exports.handler = function(event, context) {
+    var php = spawn('php-71-bin/bin/php', ['htdocs/index-silex.php'], {
+      env: {
+        REQUEST_URI: (event.params&&event.params.proxy?'/'+event.params.proxy+'/':''),
+        bigprimesDBEndPoint: '${var.rdshost}',
+        bigprimesDBName: '${var.rdsdb}',
+        bigprimesDBUser: '${var.rdsuser}',
+        bigprimesDBPass: '${var.rdspass}'
+      }
+    });
+    var output = '';
+
+    //send the input event json as string via STDIN to php process
+    php.stdin.write(JSON.stringify(event));
+
+    //close the php stream to unblock php process
+    php.stdin.end();
+
+    //dynamically collect php output
+    php.stdout.on('data', function(data) {
+          output+=data;
+    });
+
+    //react to potential errors
+    php.stderr.on('data', function(data) {
+            console.log("STDERR: "+data);
+    });
+
+    //finalize when php process is done.
+    php.on('close', function(code) {
+            context.succeed(output);
+    });
+}
+EOF
+  filename = "php.js"
 }
 
 resource "aws_cloudformation_stack" "api" {
