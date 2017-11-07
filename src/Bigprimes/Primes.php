609a0726be58b34290f3f193930b6901dd469750
Craig Mayhew <craig@mayhew.io>
2017-11-07 22:08:09 +0000

    Refactored factorial code. Added tests for factorial methods.

--- src/Bigprimes/Primes.php
+++ src/Bigprimes/Primes.php
@@ -248,4 +248,31 @@ class Primes
     {
         return ($this->maxPrime / 100);
     }
+
+    public function bcfact($n){
+      if ((string)$n === '0') {
+        return '1';
+      }
+
+      $factorial = (string)$n;
+      while (--$n > 1) {
+        $factorial = bcmul($factorial, (string)$n);
+      }
+      return $factorial;
+    }
+
+    /*
+     *
+     */
+    public function isFactorial($number)
+    {
+      $f = 1;
+      for ($attempt = 1; $f <= $number; $attempt++) {
+        $f = $this->bcfact($attempt);
+        if ($f === $number) {
+          return $attempt;
+        }
+      }
+      return false; 
+    }
 }
---
+++ test/Primes.php
@@ -0,0 +1,26 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../src/Bigprimes/Pages.php';
+
+class test_Primes extends TestCase
+{
+  public function test_factorial()
+  {
+    $p = new \Bigprimes\Primes(new \stdClass());
+
+    $this->assertEquals('1', $p->bcfact(0));
+    $this->assertEquals('1', $p->bcfact(1));
+    $this->assertEquals('2', $p->bcfact(2));
+    $this->assertEquals('6', $p->bcfact(3));
+    $this->assertEquals('24', $p->bcfact(4));
+    $this->assertEquals('15882455415227429404253703127090772871724410234473563207581748318444567162948183030959960131517678520479243672638179990208521148623422266876757623911219200000000000000000000000000', $p->bcfact(110));
+
+    $this->assertEquals('1', $p->isFactorial('1'));
+    $this->assertEquals('2', $p->isFactorial('2'));
+    $this->assertEquals('3', $p->isFactorial('6'));
+    $this->assertEquals('4', $p->isFactorial('24'));
+    $this->assertEquals('110', $p->isFactorial('15882455415227429404253703127090772871724410234473563207581748318444567162948183030959960131517678520479243672638179990208521148623422266876757623911219200000000000000000000000000'));
+  }
+}
