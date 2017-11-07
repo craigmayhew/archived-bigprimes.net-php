df1311137a06de94ef347761f9a46cfc456dbe16
Craig Mayhew <craig@mayhew.io>
2017-11-07 22:44:06 +0000

    Moved prob_prime() and added unit test

--- test/Primes.php
+++ test/Primes.php
@@ -23,4 +23,18 @@ class test_Primes extends TestCase
     $this->assertEquals('4', $p->isFactorial('24'));
     $this->assertEquals('110', $p->isFactorial('15882455415227429404253703127090772871724410234473563207581748318444567162948183030959960131517678520479243672638179990208521148623422266876757623911219200000000000000000000000000'));
   }
+
+  public function test_prob_prime()
+  {
+    $p = new \Bigprimes\Primes(new \stdClass());
+
+    // prime checks
+    $this->assertEquals(true, $p->prob_prime('2'));
+    $this->assertEquals(true, $p->prob_prime('7'));
+    $this->assertEquals(true, $p->prob_prime('39850859372740856242099725857146778412298219829168807875828109446556425650123663580223371858956633234197793823836800702258795237043648054470513616206175403378621362265360297207018853335998176098526621'));
+    // none primes
+    $this->assertEquals(false, $p->prob_prime('90'));
+    // known false positives:
+    $this->assertEquals(true, $p->prob_prime('427'));
+  }
 }
