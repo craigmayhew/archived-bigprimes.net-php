9f51de75755d9015e3c935265b063da0d33697a4
Craig Mayhew <craig@mayhew.io>
2018-03-25 16:28:45 +0100

    Added h1

--- src/Bigprimes/Pages/Contact.php
+++ src/Bigprimes/Pages/Contact.php
@@ -6,6 +6,6 @@ class Contact extends \Bigprimes\Pages
     public function getContent()
     {
         return
-        'Please contact me via <a href="https://twitter.com/craigmayhew">twitter</a> or by opening an issue on <a href="https://github.com/craigmayhew/bigprimes.net/">github</a>';
+        '<h1>Contact</h1><br><br><br>Please contact me via <a href="https://twitter.com/craigmayhew">twitter</a> or by opening an issue on <a href="https://github.com/craigmayhew/bigprimes.net/">github</a>';
     }
 }
