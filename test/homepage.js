3bdd6353e62c135ae93f4f5b12ad6240b86d24b3
Craig Mayhew <craig@mayhew.io>
2018-09-09 22:54:42 +0100

    Added cruncher form submission test

--- test/homepage.js
+++ test/homepage.js
@@ -2,7 +2,7 @@ var config = require('../nightwatch.conf.js');
 const chromedriver = require("chromedriver");
 
 module.exports = {
-    'Homepage' : function (client) {
+   'Homepage' : function (client) {
       client
         .url('https://www.bigprimes.net')
         .waitForElementVisible('body', 1000)
@@ -11,5 +11,24 @@ module.exports = {
         .assert.containsText('#content > table > tbody > tr',
           'Date News')
         .end();
+    },
+
+    'Number Cruncher' : function (client) {
+        client
+            .url('https://www.bigprimes.net/cruncher/')
+            .waitForElementVisible('body', 1000)
+            .assert.visible('#content')
+            //click the textarea
+            .waitForElementVisible('#content > div > form > div > textarea', 1000)
+            .execute('$("#content > div > form > div > textarea").val("33")')
+            //.setValue('#content > div > form > div > textarea','33')
+            //submit form
+            .click('input[type=button]')
+            .pause(1000)
+            .assert.containsText('h1', '33 - thirty three')
+            .waitForElementVisible('#content > div > table:nth-child(16) > tbody > tr:nth-child(1) > td:nth-child(2)', 10000)
+            .assert.containsText('#content > div > table:nth-child(16) > tbody > tr:nth-child(1) > td:nth-child(2)',
+            'XXXIII')
+            .end();
     }
 };
