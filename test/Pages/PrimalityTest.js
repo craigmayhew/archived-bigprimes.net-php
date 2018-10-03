7007268cc5eea44f2185130d8d8965e1b95e8f93
Craig Mayhew <craig@mayhew.io>
2018-10-03 20:47:53 +0100

    Failing test

---
+++ test/Pages/PrimalityTest.js
@@ -0,0 +1,20 @@
+var config = require('../../nightwatch.conf.js');
+const chromedriver = require("chromedriver");
+
+module.exports = {
+   'PrimalityTest' : function (client) {
+      client
+        .url('https://www.bigprimes.net/primalitytest/')
+        .waitForElementVisible('body', 1000)
+        .assert.title('Big Primes: Browser Powered Primality Test')
+        //.waitForElementVisible('input[name=primes]')
+          //.moveTo('input[name=primes]')
+          //.click('input[name=primes]')
+          //.keys('1')
+        //.execute('$("input[name=primes]").val("1")')
+        //.execute('$("input[name=start]").val("77777")')
+        //.click('form[name=primelist] > input[type="button"]:nth-child(5)')
+        //.assert.containsText('#javascriptlistoutput', "77783 is a (proven) prime!", 1000)
+        .end();
+    }
+};
