fa770d70d0f9edfadfdcd6b3858c53adcf9e3dcc
Craig Mayhew <craig@mayhew.io>
2018-10-03 22:32:19 +0100

    attempt5

--- test/Pages/PrimalityTest.js
+++ test/Pages/PrimalityTest.js
@@ -8,9 +8,9 @@ module.exports = {
         .waitForElementVisible('body', 1000)
         .assert.title('Big Primes: Browser Powered Primality Test')
         .waitForElementVisible('input[name=primes]')
-        .execute('$("input[name=primes]").val("1")')
+        .execute($("input[name=primes]").val("1"))
         .waitForElementVisible('input[name=start]')
-        .execute('$("input[name=start]").val("77777")')
+        .execute($("input[name=start]").val("77777"))
         .click('form[name=primelist] > input[type=button]:nth-child(5)')
         .assert.containsText('#javascriptlistoutput', "77783 is a (proven) prime!", 1000)
         .end();
