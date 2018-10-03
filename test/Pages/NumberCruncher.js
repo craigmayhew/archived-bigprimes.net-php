161d5154596c75e5d86320f8e6f96a7aa21d50b2
Craig Mayhew <craig@mayhew.io>
2018-10-03 19:11:49 +0100

    Removed setvalue()

--- test/Pages/NumberCruncher.js
+++ test/Pages/NumberCruncher.js
@@ -10,7 +10,6 @@ module.exports = {
             //click the textarea
             .waitForElementVisible('#content > div > form > div > textarea', 1000)
             .execute('$("#content > div > form > div > textarea").val("33")')
-            //.setValue('#content > div > form > div > textarea','33')
             //submit form
             .click('input[type=button]')
             .pause(1000)
