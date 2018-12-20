d33a3472f81218562c8c67d433920c869df61fa1
Craig Mayhew <craig@mayhew.io>
2018-12-20 00:43:08 +0000

    Removed unlock from geth command, we can do the unlock later

--- contracts/deploy-rinkeby.sh
+++ contracts/deploy-rinkeby.sh
@@ -1,11 +1,7 @@
-# save our geth password to a file
-echo $RINKEBY_PRIVATE_PASS > gethpasword.file
 # load up our test wallet
 geth --rinkeby --cache 1024 --nousb --syncmode light --unlock $RINKEBY_PUBLIC_ETH_ADDRESS --password gethpassword.file --rpc --rpcapi eth,web3,personal &
 # sleep to allow rinkeby to connect
 sleep 10s
-# remove password file
-rm gethpasword.file
 # attempt to use geth
 geth --rinkeby --exec "eth.getGasPrice(function(e,r){console.log(r)})" attach
 
@@ -14,6 +10,6 @@ geth --rinkeby --exec "eth.getGasPrice(function(e,r){console.log(r)})" attach
 #storageContractAbi = storageOutput.contracts['33.sol:ethForAnswersBounty'].abi
 #storageContract = eth.contract(JSON.parse(storageContractAbi))
 #storageBinCode = "0x" + storageOutput.contracts['33.sol:ethForAnswersBounty'].bin
-#personal.unlockAccount(eth.accounts[0])
+#personal.unlockAccount(eth.accounts[0], $RINKEBY_PRIVATE_PASS)
 #deployTransactionObject = { from: eth.accounts[0], data: storageBinCode, gas: 1000000 }
 #storageInstance = storageContract.new(deployTransactionObject)
