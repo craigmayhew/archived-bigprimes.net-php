96ed6a726710e19da0b8a57b189986a18e1c9c30
Craig Mayhew <craig@mayhew.io>
2018-12-20 00:48:10 +0000

    Corrected previous commit so now really don't try to unlock an eth account when opening geth

--- contracts/deploy-rinkeby.sh
+++ contracts/deploy-rinkeby.sh
@@ -1,5 +1,5 @@
 # load up our test wallet
-geth --rinkeby --cache 1024 --nousb --syncmode light --unlock $RINKEBY_PUBLIC_ETH_ADDRESS --password gethpassword.file --rpc --rpcapi eth,web3,personal &
+geth --rinkeby --cache 1024 --nousb --syncmode light --rpc --rpcapi eth,web3,personal &
 # sleep to allow rinkeby to connect
 sleep 10s
 # attempt to use geth
@@ -10,6 +10,6 @@ geth --rinkeby --exec "eth.getGasPrice(function(e,r){console.log(r)})" attach
 #storageContractAbi = storageOutput.contracts['33.sol:ethForAnswersBounty'].abi
 #storageContract = eth.contract(JSON.parse(storageContractAbi))
 #storageBinCode = "0x" + storageOutput.contracts['33.sol:ethForAnswersBounty'].bin
-#personal.unlockAccount(eth.accounts[0], $RINKEBY_PRIVATE_PASS)
+#personal.unlockAccount($RINKEBY_PUBLIC_ETH_ADDRESS, $RINKEBY_PRIVATE_PASS)
 #deployTransactionObject = { from: eth.accounts[0], data: storageBinCode, gas: 1000000 }
 #storageInstance = storageContract.new(deployTransactionObject)
