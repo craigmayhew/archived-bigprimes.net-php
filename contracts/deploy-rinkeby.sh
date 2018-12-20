a18533c5ab6eb43db385a1fb5938fc89b481208a
Craig Mayhew <craig@mayhew.io>
2018-12-20 09:49:42 +0000

    Commands now placed in JS file and run to deploy contract. Likely will fail without local test account setup.

--- contracts/deploy-rinkeby.sh
+++ contracts/deploy-rinkeby.sh
@@ -7,12 +7,19 @@ sleep 40s
 geth --rinkeby --exec "eth.getGasPrice(function(e,r){console.log('gas price: ',r)})" attach
 geth --rinkeby --exec "console.log('last block: ',eth.blockNumber)" attach
 
-# 33.sol
-#solc --optimize --combined-json abi 33.sol > 33.json
-#storageOutput = `solc --optimize --combined-json abi 33.sol`
-#storageContractAbi = storageOutput.contracts['33.sol:ethForAnswersBounty'].abi
-#storageContract = eth.contract(JSON.parse(storageContractAbi))
-#storageBinCode = "0x" + storageOutput.contracts['33.sol:ethForAnswersBounty'].bin
-#personal.unlockAccount($RINKEBY_PUBLIC_ETH_ADDRESS, $RINKEBY_PRIVATE_PASS)
-#deployTransactionObject = { from: eth.accounts[0], data: storageBinCode, gas: 1000000 }
-#storageInstance = storageContract.new(deployTransactionObject)
+# compile 33.sol
+echo 'storageOutput = ' > 33.json
+solc --optimize --combined-json --abi --bin 33.sol >> 33.json
+# write js deployment script for 33.sol
+cat > /tmp/33.js <<EOL
+storageContractAbi = storageOutput.contracts['33.sol:ethForAnswersBounty'].abi
+storageContract = eth.contract(JSON.parse(storageContractAbi))
+storageBinCode = "0x" + storageOutput.contracts['33.sol:ethForAnswersBounty'].bin
+personal.unlockAccount($RINKEBY_PUBLIC_ETH_ADDRESS, $RINKEBY_PRIVATE_PASS)
+deployTransactionObject = { from: eth.accounts[0], data: storageBinCode, gas: 1000000 }
+storageInstance = storageContract.new(deployTransactionObject)
+EOL
+# run js deployment script for 33.sol
+geth --rinkeby --exec 'loadscript("/tmp/33.js")' attach
+# cleanup for 33.sol
+rm /tmp/33.js
