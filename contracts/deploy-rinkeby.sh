7685ab55c60ccb975574e3053f410be88559c54d
Craig Mayhew <craig@mayhew.io>
2018-12-20 09:02:55 +0000

    Geth now outputs block number

--- contracts/deploy-rinkeby.sh
+++ contracts/deploy-rinkeby.sh
@@ -2,10 +2,13 @@
 geth --rinkeby --cache 1024 --nousb --syncmode light --rpc --rpcapi eth,web3,personal &
 # sleep to allow rinkeby to connect
 sleep 10s
+
 # attempt to use geth
-geth --rinkeby --exec "eth.getGasPrice(function(e,r){console.log(r)})" attach
+geth --rinkeby --exec "eth.getGasPrice(function(e,r){console.log('gas price: ',r)})" attach
+geth --rinkeby --exec "console.log('last block: ',eth.blockNumber)" attach
 
 # 33.sol
+#solc --optimize --combined-json abi 33.sol > 33.json
 #storageOutput = `solc --optimize --combined-json abi 33.sol`
 #storageContractAbi = storageOutput.contracts['33.sol:ethForAnswersBounty'].abi
 #storageContract = eth.contract(JSON.parse(storageContractAbi))
